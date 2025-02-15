e234f9f680157c662b1f3a2cb5c2d4f20e3ac050
success-techy <127170918+success-techy@users.noreply.github.com>
2025-02-15 22:12:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -110,4 +110,4 @@ def handle_disconnect():
         emit('update_users', online_users, broadcast=True)
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
+    socketio.run(app, host='0.0.0.0', port=5000, debug=True, allow_unsafe_werkzeug=True)
